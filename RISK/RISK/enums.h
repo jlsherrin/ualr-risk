aec263da8a55797b941288b271459291f728c24e
jlsherrin@gmail.com <jlsherrin@gmail.com@ba2fcf0f-8572-9b5b-16fa-683bdfc7f229>
2011-12-12 22:15:31 +0000

    Adding enums.h.

---
+++ RISK/RISK/enums.h
@@ -0,0 +1,11 @@
+#ifndef Enums_H
+#define Enums_H
+
+enum AttackModes{ATTACKMODE,DEFENDMODE};
+enum GamePhases{INITIAL,NORMAL,VICTORY};
+enum SubGamePhases{CARDS,PLACEMENT,ATTACK,FORTIFY,DONE};
+enum AttackResult{PUSH,WIN,LOSE,ATK_ERROR};
+
+#endif
+
+
